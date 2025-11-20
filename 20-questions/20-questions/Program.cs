54e63a4241167e332536ac663f3080d5be4b03a9
Meghan <meghantomback@gmail.com>
2025-11-20 08:44:24 -0500

    stubbed out

--- 20-questions/20-questions/Program.cs
+++ 20-questions/20-questions/Program.cs
@@ -1,1 +1,41 @@
-﻿
+﻿using System.Xml.Linq;
+//import system io
+
+namespace _20_questions
+{
+    internal class Program
+    {
+        //first node
+        //stream reader -> get the questions our
+        //stream write -> "learning" portion 
+        static void Main(string[] args)
+        {
+            //while loop -> run until user input is defined end case
+
+            //instatiate current response + line number
+                //while the response value isn't predefined yes or no case
+                    //ask the first node question
+                    //get user input
+                //if win
+                    //you win
+                //if fail
+                    //what question should be added + anaswers
+                    //write that to file
+
+        }
+    }
+
+    //TreeNode class
+
+    //data -> question / answer text
+    //yes path node
+    //no path node
+
+    //in the file
+    //each node is its own line
+    //question @ yes line number @ no line number
+    
+    //pre define a string that goes for the yes and no of a final guess
+    //on the predetermined no thing -> trigger new question input stuff
+}
+
